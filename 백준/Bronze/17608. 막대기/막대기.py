ced2a058e650a419b1a7b9df717c0b87e8bc7975
HY25N <wngus8025@gmail.com>
2026-01-24 22:06:36 +0900

    막대기 재풀이
    막대기 문제 재풀이 코드

--- 백준/Bronze/17608. 막대기/막대기.py
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -1,3 +1,4 @@
+# 2025-07-29
 import sys
 input = sys.stdin.readline
 
@@ -11,4 +12,20 @@ for i in reversed(range(N)):
     if sticks[i] > max_height:
         count += 1
         max_height = sticks[i]
+
 print(count)
+
+# 2026-01-24 해당 문제 재풀이
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+lst = [int(input()) for _ in range(N)]
+
+stack = []
+
+for h in reversed(lst):
+    if not stack or h > stack[-1]:
+        stack.append(h)
+
+print(len(stack))
