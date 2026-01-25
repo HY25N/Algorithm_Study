ceddabd768c4e85f093d1168ac341838fba52122
HY25N <wngus8025@gmail.com>
2026-01-25 21:59:21 +0900

    Refactor word reversal logic in 단어순서 뒤집기.py
    Refactored code to improve readability and added comments.

--- 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.py
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.py
@@ -1,3 +1,12 @@
 for i in range(int(input())):
     n = input().split()
+
     print(f"Case #{i+1}: {' '.join(n[::-1])}")
+
+# 2026-01-25 (재풀이)
+N = int(input())
+
+for i in range(N):
+    L = input().split()
+    reversed_L = L[::-1]
+    print(f"Case #{i+1}: {' '.join(reversed_L)}")
